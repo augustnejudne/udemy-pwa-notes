b8835145f16fd7e34baac64db0eae02b4c7a9ef8
augustnejudne <august.nejudne@gmail.com>
2018-03-19 19:44:47 +0800

    march 19 2018

---
+++ section 3 - the service workers/public/sw.js
@@ -0,0 +1,9 @@
+
+self.addEventListener('install', (event) => {
+  console.log('[service worker] Installing service worker ...', event)
+})
+
+self.addEventListener('activate', (event) => {
+  console.log('[service worker] Activating service worker ...', event)
+  return self.clients.claim()
+})
